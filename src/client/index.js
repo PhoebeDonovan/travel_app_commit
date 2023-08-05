7c555423cb20da61472e6058072ec02e463ebb28
PhoebeDonovan <127471962+PhoebeDonovan@users.noreply.github.com>
2023-08-05 10:55:14 +1000

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,9 @@ import { generateButtonClicked } from './js/app';
 import { addEntry } from './js/app';
 import '../client/styles/style.css'
 
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', generateButtonClicked);
+
 console.log(generateButtonClicked);
 
 export { 
